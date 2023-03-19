466021da50f7a522c19fb0cc60c584d0bd50ee0d
jeeva9949 <goddumarrijeevananda@gmail.com>
2023-03-19 10:30:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Increment, Decrement, Multiplication,Division } from "./actions/index";
+
+
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  const mystate1 = useSelector((state) => state.ChangeNumber);
+  const mystate2 = useSelector((state) => state.ALU);
+  const dispatch = useDispatch()
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App1">
+        <h1>Redux</h1>
+        <div className="App">
+          <button onClick={() => dispatch(Increment())}>+</button>
+          <p>{mystate1}</p>
+          <button onClick={() => dispatch(Decrement())}>-</button>
+        </div>
+      </div>
+      <div className="App1">
+        <h1>Redux</h1>
+        <div className="App">
+          <button onClick={() => dispatch(Multiplication())}>*</button>
+          <p>{mystate2}</p>
+          <button onClick={() => dispatch(Division())}>/</button>
+        </div>
+      </div>
+    </>
   );
 }
 
